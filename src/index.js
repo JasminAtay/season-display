520958b10310d2f7d22ffa457390a7e87dd87b18
Jasmin Atay <yasematayy@gmail.com>
2021-03-09 21:09:29 +0100

    State is used in a class comp. & constructor

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
 //1- Kullanilacak library import ederiz
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+
 //2- Component olusturuyoruz
-const App = () =>{
-    navigator.geolocation.getCurrentPosition(
-    //success
-    (location)=>{console.log("enlem :" ,location.coords.latitude)},
-    //error
-    (error)=>console.log(`error message: `, error.message)
-    )
-    return(
-        <div>
-            <h1 style={{textAlign: 'center'}}>this is season app</h1>
-        </div>
-    )
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { location: null, error: null }
+
+        navigator.geolocation.getCurrentPosition(
+            //success
+            (location) => {
+                this.setState({ location: location.coords.latitude })
+                console.log(`location: `, this.state.location);
+            },
+            //error
+            (error) => {
+                this.setState({ error: error.message });
+                console.log(`error Message: `, this.state.error);
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <h1 style={{ textAlign: 'center' }}>this is season app</h1>
+            </div>
+        );
+    }
 }
 
 //3- Export edilecek
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'))
