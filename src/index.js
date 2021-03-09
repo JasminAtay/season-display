fc94cd0794af1e768c52265292047109b062470a
Jasmin Atay <yasematayy@gmail.com>
2021-03-09 20:38:47 +0100

    Geolocation api is implemented

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom'
 
 //2- Component olusturuyoruz
 const App = () =>{
+    navigator.geolocation.getCurrentPosition(
+    //success
+    (location)=>{console.log("enlem :" ,location.coords.latitude)},
+    //error
+    (error)=>console.log(`error message: `, error.message)
+    )
     return(
         <div>
             <h1 style={{textAlign: 'center'}}>this is season app</h1>
